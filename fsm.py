05507d1faa242f65f108d78f2250df633a3e46d5
vickyyeh <vicky8762330@gmail.com>
2019-12-11 03:44:33 +0800

    Add menu ui

--- fsm.py
+++ fsm.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify, request, abort, send_file
 from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 
 from utils import send_text_message
 
