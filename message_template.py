3df3ca17c72bc9a85f48e48498e19f630e614d5e
vickyyeh <vicky8762330@gmail.com>
2019-12-11 03:24:25 +0800

    Add menu ui

---
+++ message_template.py
@@ -0,0 +1,118 @@
+
+main_menu = {
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/V2tkpQb.png",
+        "size": "full",
+        "aspectMode": "fit",
+        "aspectRatio": "1.25:1"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "即時查詢",
+              "text": "查詢即時匯率"
+            },
+            "height": "md",
+            "color": "#ff6666",
+            "style": "primary"
+          }
+        ],
+        "spacing": "lg"
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/nQaCDXh.png",
+        "size": "full",
+        "aspectMode": "fit",
+        "aspectRatio": "1.25:1"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "近期趨勢圖",
+              "text": "查詢趨勢走向"
+            },
+            "height": "md",
+            "color": "#ff66b3",
+            "style": "primary"
+          }
+        ],
+        "spacing": "lg"
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/UrSkoW4.png",
+        "size": "full",
+        "aspectMode": "fit",
+        "aspectRatio": "1.25:1"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "推薦與否",
+              "text": "是否推薦兌幣"
+            },
+            "height": "md",
+            "color": "#b366ff",
+            "style": "primary"
+          }
+        ],
+        "spacing": "lg"
+      }
+    },
+    {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url": "https://i.imgur.com/lFq9NTg.jpg",
+        "size": "full",
+        "aspectMode": "fit",
+        "aspectRatio": "1.25:1"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "介紹與說明",
+              "text": "功能介紹與說明"
+            },
+            "height": "md",
+            "color": "#66b3ff",
+            "style": "primary"
+          }
+        ],
+        "spacing": "lg"
+      }
+    }
+  ]
+}
